ae97a922a3cd4d3ae3b1f3c0f45a2f8d90d5e9b6
Yevhenii Orlov <62546115+CoachJohnsy@users.noreply.github.com>
2021-12-29 15:04:49 +0200

    started adding lab-3 by gui

---
+++ lab-3/constants.py
@@ -0,0 +1,10 @@
+MENU_ROWS = [(0, "Show one table"),
+             (1, "Show all tables"),
+             (2, "Insert data"),
+             (3, "Delete data"),
+             (4, "Update data"),
+             (5, "Exit")]
+
+MENU_COLUMNS =\
+    ["Command",
+     "Action"]
